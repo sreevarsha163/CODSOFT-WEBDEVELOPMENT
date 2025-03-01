7d21b2be7dbea5a045e14a6f87a3eb1e07150745
sreevarsha163 <varshasree163@gmail.com>
2025-03-01 12:42:11 +0530

    Add files via upload

---
+++ calculator/calculatorscript.js
@@ -0,0 +1,35 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function addKeyboardSupport(event) {
+    const key = event.key;
+    if (/[0-9+\-*/.=]/.test(key)) {
+        appendToDisplay(key);
+    } else if (key === 'Enter') {
+        calculateResult();
+    } else if (key === 'Backspace') {
+        deleteLast();
+    } else if (key === 'Escape') {
+        clearDisplay();
+    }
+}
+
+document.addEventListener('keydown', addKeyboardSupport);
